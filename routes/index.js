0cf984d16c4ab4b95cdf96f66be6322d9c8aee82
weasterner <yingjin.us@gmail.com>
2016-11-21 16:48:23 -0500

    Add bubble head mutations generation logic. Note the performance of jimp can be slow

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var fs = require('fs');
 var jimp = require('jimp');
 var oxford = require('project-oxford');
-var GIFEncoder = require('GIFEncoder');
+var GIFEncoder = require('gifencoder');
 var pngFileStream = require('png-file-stream');
 
 
@@ -20,7 +20,14 @@ router.post('/',(req,res,next) => {
       return oxfordClient.face.detect({path:image,analyzesAge:true, analyzesGender:true});
     })
     .then(function generateBubblePermutations(response){
-      console.log("TODO: generate multiple images with head rotated");
+      promises=[];
+      degrees = [10,0,-10];
+
+      for(var i=0; i<degrees.length; i++){
+        var outputName = req.file.path +'_' + i + '.png';
+        promises.push(cropHeadAndPasteRotated(req.file.path, response[0].faceRectangle,degrees[i], outputName))
+      }
+      return Promise.all(promises);
     })
     .then(function generateGif(dimensions){
       console.log("TODO: generate GIF");
@@ -30,4 +37,29 @@ router.post('/',(req,res,next) => {
       res.render('index', {title:'Done!', image: gifLocation})
     } );
 })
+
+function cropHeadAndPasteRotated(inputFile, faceRectangle, degrees, outputName){
+  return new Promise((resolve, reject) => {
+    jimp.read(inputFile).then((image) => {
+      // Face detection only captures a small portion of the face,
+      // so compensate for this by expanding the area appropriately.
+      var height = faceRectangle['height'];
+      var top = faceRectangle['top']- height*0.5;
+      height*= 1.6;
+      var left = faceRectangle['left'];
+      var width = faceRectangle['width'];
+      // Crop head, scale up slightly, rotate, and paste on original image
+      image.crop(left, top, height, width)
+        .scale(1.05)
+        .rotate(degrees, (err, rotated) => {
+          jimp.read(inputFile).then((original) => {
+            original.composite(rotated, left-0.1*width, top-0.05*height)
+              .write(outputName,() => {
+                resolve([original.bitmap.width, original.bitmap.height]);
+              });
+          });
+        });
+    });
+  });
+}
 module.exports = router;
